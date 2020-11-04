84ab5eb6ecf6d9214f0adc479c463c3fc4e364e3
Nik Sofianos <nsofianos@hotmail.com>
2020-11-04 00:21:27 +0000

    refactored to use test imports

--- head.js
+++ head.js
@@ -1,9 +1,6 @@
-const assertEqual = require('./assertEqual');
-
-
 const head = function(arr) {
   let firstArg = arr[0];
   return firstArg;
 };
 
-assertEqual(head([0,1,2]), 0);
+module.exports = head;
--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔⛔⛔ Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const tail = function(arr) {
   let newArr = [];
   for (let i = 1; i < arr.length; i++) {
@@ -15,17 +6,6 @@ const tail = function(arr) {
   return newArr;
 };
 
-const result = tail([1,2,3]);
-const numbers = [2,3,4];
-tail(numbers);
+module.exports = tail;
 
-assertEqual(result.length, 2);
-assertEqual(result[0], 2);
-assertEqual(result[1], 3);
-
-assertEqual(numbers.length, 3);
-
-assertEqual(tail([5]), 0);
-assertEqual(tail([]), 0);
-tail([]);
 
